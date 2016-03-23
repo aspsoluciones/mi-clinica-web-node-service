70584a701a0027debb651eaf6e2a1765892c7f52
epotignano <epotignano@gmail.com>
2016-03-23 15:18:15 -0300

    Intregrated Cloduinary To queue

--- server/modules/queues/jobs/uploadImage.js
+++ server/modules/queues/jobs/uploadImage.js
@@ -10,16 +10,19 @@ var Firebase = require('firebase');
 /** init cloudinary configuration **/
 cloudinary.config(process.env.CLOUDINARY);
 
-module.exports =  function(imageBinary, collection, entityRef, saveInKey) {
-    var _base64String = btoa(imageBinary);
+module.exports =  function(data, progress, resolve, reject) {
+    var _base64String = btoa(data.file);
     _base64String = 'data:image/png;base64,' + _base64String;
     cloudinary.uploader.upload(_base64String, function(result){
-        var _entityInstance = new Firebase(process.env.FIREBASE_URL + '/' + collection + '/' + entityRef);
+        var _entityInstance = new Firebase(process.env.FIREBASE_URL + '/' + data.collection + '/' + data.entityId);
         if(!result.error) {
             _entityInstance.update({
-                [saveInKey] : result
+                [data.saveInKey] : result
             })
-        };
+            resolve('Image uploaded without problems')
+        } else {
+            reject('Error uploading image to Cloudinary');
+        }
         /*_entityInstance.child('doneJobs').push({
          type: 'IMAGE_UPLOAD',
          key: saveInKey,
--- server/modules/queues/service.js
+++ server/modules/queues/service.js
@@ -6,61 +6,29 @@ var Queue = require('firebase-queue'),
 // The location of the Queue - can be any Firebase Location
 var ref = new Firebase('https://' + process.env.FB_NAME + '.firebaseio.com/');
 // Creates the Queue
-var options = {
-    specId: 'sanitize_message',
-
+var queueOptions = {
+    'specId': 'def',
+    'numWorkers': 10,
 };
+
 
 var jobs = requireDir('./jobs');
 
 var queueRef = ref.child('queue');
-queueRef.child('specs').set({
-    sanitize_message: {
-        in_progress_state: "sanitize_message_in_progress",
-        finished_state: "sanitize_message_finished"
-    },
-    fanout_message: {
-        start_state: "sanitize_message_finished",
-        in_progress_state: "fanout_message_in_progress",
-        error_state: "fanout_message_failed",
-        retries: 3
-    }
-});
 
-// Add tasks onto the queue
-var taskNumber = 0;
-setInterval(function() {
-    queueRef.child('tasks').push({
-        message: 'Hello Firebase',
-        name: "¨Potignanoさん"
-    });
-}, 1000);
-
-var sanitizeQueue = new Queue(queueRef, options, function(data, progress, resolve, reject) {
+var queue = new Queue(queueRef, queueOptions, function(data, progress, resolve, reject) {
     // Read and process task data
-    console.log(data);
-    console.log(jobs);
-
-    // Do some work
-    var percentageComplete = 0;
-    var interval = setInterval(function() {
-        percentageComplete += 20;
-        if (percentageComplete >= 100) {
-            clearInterval(interval);
-        } else {
-            progress(percentageComplete);
-        }
-    }, 1000);
-
-    // Finish the task
-    setTimeout(function() {
-        resolve();
-    }, 5000);
+    var _spec = jobs[data.jobType];
+    if(_spec) {
+        _spec(data, progress, resolve, reject);
+    } else {
+        reject('Job spec unknown or unspecified');
+    }
 });
 
 process.on('SIGINT', function() {
     console.log('Starting queue shutdown');
-    sanitizeQueue.shutdown().then(function() {
+    queue.shutdown().then(function() {
         console.log('Finished queue shutdown');
         process.exit(0);
     });
